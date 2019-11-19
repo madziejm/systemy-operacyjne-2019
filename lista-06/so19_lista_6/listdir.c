23b9b16250940cab0c413289c280bedece9a3165
Maciej M <22326971+madziejm@users.noreply.github.com>
2019-11-19 21:35:11 +0100

    Tasklist 06: correct bits in `listdir.c`

--- lista-06/so19_lista_6/listdir.c
+++ lista-06/so19_lista_6/listdir.c
@@ -33,9 +33,9 @@ static void print_mode(mode_t m) {
   char ox = (m & S_IXOTH) ? 'x' : '-';
 
   /* TODO: Fix code to report set-uid/set-gid/sticky bit as 'ls' does. */
-  ux = (m & S_ISUID) ? ('S' : 's') : ux;
-  gx = (m & S_ISGID) ? ('S' : 's') : gx;
-  ox = (m & S_ISVTX) ? ('T' : 't') : ox;
+  ux = (m & S_ISUID) ? (S_IXUSR & m ? 'S' : 's') : ux;
+  gx = (m & S_ISGID) ? (S_IXUSR & m ? 'S' : 's') : gx;
+  ox = (m & S_ISVTX) ? (m & S_IXUSR ? 't' : 'T') : ox;
 
   printf("%c%c%c%c%c%c%c%c%c%c", t, ur, uw, ux, gr, gw, gx, or, ow, ox);
 }
